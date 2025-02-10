4f0db3a71c5cc8931cfbc286c4bc910983c3a7f7
Cristian Arana <cris171095@gmail.com>
2025-02-09 21:41:39 -0400

    feature: create hero section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				headerGreen: '#6bdb50',
+				headerWhite: '#fdfffe',
+				headerBlue: '#075fb4',
+				headerText: '#4a544c',
+				headerButtonText: '#f4f6f6',
+			},
+		},
 	},
 	plugins: [],
 }
